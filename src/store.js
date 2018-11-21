2760a400f3251bea935ffb10dbe68a38c0157c00
Magnus Jonsson <magnus@hpc2n.umu.se>
2018-11-21 14:34:46 +0100

    socket.io enabled store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+const votes = {
   state: {
-
+    votes: {},
+    isConnected: false,
   },
   mutations: {
+    SOCKET_CONNECT(state, status) {
+      //console.log("Connect: " + status);
+      state.isConnected = true;
+    },
+    SOCKET_REGISTER(state, data) {
+      //console.log("id: " + data['id'] + " name: " + data['name']);
+      let name = data['name'];
+      let id = data['id'];
+      if(state.votes[id] === undefined) {
+        Vue.set(state.votes,id,{ name, votes: [] });
+      } else {
+        Vue.set(state.votes[id],'name',name);
+      }
+    },
+    SOCKET_VOTE(state, data) {
+      let id = data['id'];
+      let number = data['number'];
+      let value = data['value'];
+      //console.log("id: " + id + " number: " + number + " value: " + value);
+      if(state.votes[id] === undefined) {
+        Vue.set(state.votes,id,{ 'name': "Unknown", votes: [] });
+      }
+      Vue.set(state.votes[id]['votes'],parseInt(number),parseInt(value));
+    }
+  }
+};
 
-  },
-  actions: {
-
+export default new Vuex.Store({
+  modules: {
+    votes,
   }
 })
