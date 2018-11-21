2805efc05a572c2b7b96862408fb7d39229612dd
Magnus Jonsson <magnus@hpc2n.umu.se>
2018-11-21 14:35:46 +0100

    Support for socket.io

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import VueSocketio from 'vue-socket.io-extended';
+import io from 'socket.io-client';
+
+// for local development...
+//Vue.use(VueSocketio, io('http://192.168.2.2:8899'), { store });
+
+Vue.use(VueSocketio, io(), { store });
+
 new Vue({
   router,
   store,
